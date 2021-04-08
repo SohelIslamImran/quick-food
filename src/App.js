b6381926722d27d26dec0a584e19846272ace6d7
Sohel Islam Imran <sohelislamimran@gmail.com>
2021-04-08 21:56:22 +0600

    Uncompleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import './App.css';
+import Banner from "./components/Banner/Banner";
+import FoodDetails from "./components/FoodDetails/FoodDetails";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Banner />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/food/:id">
+          <FoodDetails />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
